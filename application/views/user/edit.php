caa1547a3330dab753c8083e28d66397d38ba3de
Max0nsky <maksimum.99@mail.ru>
2020-05-04 13:55:16 +0300

    Восстановление редактирования пользователя

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -22,8 +22,12 @@ $User = Config::getObject('core.user.class');
     <h5>Введите пароль</h5>
     <input type="text" name="pass" placeholder="новый пароль" value=""><br>
     <h5>Введите e-mail</h5>
-    <input type="text" name="email"  placeholder="email" value="<?= $viewAdminusers->email ?>"><br>
-    
+    <input type="text" name="email"  placeholder="email" value="<?= $viewAdminusers->email ?>"><br> 
+    <label for="role"><h5>Права доступа</h5></label>
+    <select name="role" id="role" class="form-control">
+        <option <?php echo (($viewAdminusers->role == "admin") ? "selected" : ""); ?> value="admin">Администратор</option>
+        <option <?php echo (($viewAdminusers->role == "auth_user") ? "selected" : ""); ?> value="auth_user">Зарегистрированный пользователь</option>
+    </select><br>
     <input type="hidden" name="id" value="<?= $_GET['id']; ?>">
     <input type="submit" name="saveChanges" value="Сохранить">
     <input type="submit" name="cancel" value="Назад">
